9c78efd58a6720fdbbd99014e31f14a8d4b41261
llya72rus <32940323+llya72rus@users.noreply.github.com>
2018-04-27 23:27:29 +0300

    Начал выполнять задание 18

--- js/map.js
+++ js/map.js
@@ -291,3 +291,71 @@ userForm.addEventListener('change', function () {
 userForm.addEventListener('submit', function () {
   document.querySelector('.success').classList.remove('hidden');
 });
+
+var MAIN_PIN_WIDTH = 65;
+var MAIN_PIN_HEIGHT = 87;
+var LIMIT_TOP_Y = 90;
+var LIMIT_BOTTOM_Y = 710;
+var LIMIT_LEFT_X = 65;
+var LIMIT_RIGHT_X = 1200;
+
+var metka = document.querySelector('.map__pin--main');
+
+metka.addEventListener('mousedown', function (evt) {
+  evt.preventDefault();
+
+  var startCoords = {
+    x: evt.clientX,
+    y: evt.clientY
+  };
+
+  var onMouseMove = function (moveEvt) {
+    moveEvt.preventDefault();
+
+    var shift = {
+      x: startCoords.x - moveEvt.clientX,
+      y: startCoords.y - moveEvt.clientY
+    };
+
+    startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    metka.style.top = (metka.offsetTop - shift.y) + 'px';
+    metka.style.left = (metka.offsetLeft - shift.x) + 'px';
+
+    var metkaLeftCoord = parseInt(metka.style.left.split('px')[0], 10);
+    var metkaTopCoord = parseInt(metka.style.top.split('px')[0], 10);
+    // console.log(metkaLeftCoord + ' ' + metkaTopCoord);
+
+    if (metkaTopCoord < LIMIT_TOP_Y - MAIN_PIN_HEIGHT) {
+      metka.style.top = LIMIT_TOP_Y - MAIN_PIN_HEIGHT + 'px';
+    }
+
+    if (metkaTopCoord > LIMIT_BOTTOM_Y - MAIN_PIN_HEIGHT) {
+      metka.style.top = LIMIT_BOTTOM_Y - MAIN_PIN_HEIGHT + 'px';
+    }
+
+    if (metkaLeftCoord < LIMIT_LEFT_X - MAIN_PIN_WIDTH) {
+      metka.style.left = LIMIT_LEFT_X - MAIN_PIN_WIDTH + 'px';
+    }
+
+    if (metkaLeftCoord > LIMIT_RIGHT_X - MAIN_PIN_WIDTH) {
+      metka.style.left = LIMIT_RIGHT_X - MAIN_PIN_WIDTH + 'px';
+    }
+
+    var showedCoordsTop = metka.offsetTop - shift.y;
+    var showedCoordsLeft = metka.offsetLeft - shift.x;
+    document.querySelector('#address').value = showedCoordsTop + 0 + ', ' + (showedCoordsLeft + 33);
+  };
+
+  var onMouseUp = function (upEvt) {
+    upEvt.preventDefault();
+    document.removeEventListener('mousemove', onMouseMove);
+    document.removeEventListener('mouseup', onMouseUp);
+  };
+
+  document.addEventListener('mousemove', onMouseMove);
+  document.addEventListener('mouseup', onMouseUp);
+});
